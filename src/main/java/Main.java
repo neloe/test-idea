0a043b580dfd049b3fc85b253187b6a9abdb3a96
Nathan Eloe <powerofazure@gmail.com>
2021-04-27 08:48:00 -0500

    Initial stuff

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args)
+    {
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Hello World");
+        int x = cin.nextInt();
+        System.out.println(x);
+    }
+}
